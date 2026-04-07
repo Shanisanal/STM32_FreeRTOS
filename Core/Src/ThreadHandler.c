d9d9acfb60c683ecb33ebe37e330a01d77591949
1950 <1950@L1673.trenser.com>
2026-04-07 14:40:25 +0530

    Function header correction

--- Core/Src/ThreadHandler.c
+++ Core/Src/ThreadHandler.c
@@ -28,7 +28,7 @@
 
 //******************************* ThreadHandlerPoller **************************
 //Purpose   : High-priority task that waits for a User Button interrupt.Processes
-//			  the hardware trigger and prepares data for the transport thread.
+//			  the hardware trigger and sends data queue to the transport thread.
 //Inputs    : pArguments - Pointer to task arguments .
 //Outputs   : None
 //Return    : None
